66f6fe137dad05e38972fbbfb87ee7cba5a30775
lukecode <lucassocorrosilva@gmail.com>
2023-05-11 20:06:33 -0300

    feat: Criando controller de autenticação

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return response()->json($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'price' => 'required|numeric|min:0',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+        $imageName = time() . '_' . $request->file('image')->getClientOriginalName();
+        $request->file('image')->storeAs('public/images', $imageName);
+
+
+        $product = new Product([
+            'name' => $validatedData['name'],
+            'image' => $imageName,
+            'price' => $validatedData['price'],
+            'category_id' => $validatedData['category_id'],
+        ]);
+
+        $product->save();
+
+        return response()->json(['message' => 'Product created successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
