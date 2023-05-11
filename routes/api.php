6cbfb04b9828119fff51498b60ca0a88e0076562
lukecode <lucassocorrosilva@gmail.com>
2023-05-11 20:49:03 -0300

    feat: Adicionar rota de registro de usuários

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/products', [ProductController::class, 'store'])->withoutMiddleware('auth');
+Route::get('/products', [ProductController::class, 'index'])->withoutMiddleware('auth');
+Route::get('/products/{id}', [ProductController::class, 'show'])->withoutMiddleware('auth');
 Route::apiResource('/categories', CategoryController::class);
+
+// Auth
+Route::post('/register', [AuthController::class, 'register']);
