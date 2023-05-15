955908a2c8986fa81b20d1770ea65afaefc66cdf
lukecode <lucassocorrosilva@gmail.com>
2023-05-15 19:22:35 -0300

    feat: Criando Model,Migration e Controller para pedidos

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Order;
+use App\Models\OrderProduct;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $order = new Order;
+        $order->user_id = Auth::user()->id;
+        $order->total = $request->total;
+        $order->save();
+
+        $id = $order->id;
+
+        $products = $request->products;
+
+        $order_product = [];
+
+        foreach ($products as $product) {
+            $order_product[] = [
+                'order_id' => $id,
+                'product_id' => $product['id'],
+                'quantity' => $product['quantity'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ];
+        }
+
+        OrderProduct::insert($order_product);
+
+        return [
+            'message' => "Pedido realizado com sucesso, estará disponível em breve!"
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Order $order)
+    {
+        //
+    }
+}
