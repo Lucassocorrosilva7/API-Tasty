7cbe46417c2d9b6dcf9664c1bdeae00f3ede7aa6
lukecode <lucassocorrosilva@gmail.com>
2023-05-16 20:29:22 -0300

    feat: Adicionando relacionamento belongsToMany com o modelo de Product

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use App\Models\OrderProduct;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\OrderCollection;
 
 class OrderController extends Controller
 {
@@ -15,7 +16,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        return new OrderCollection(Order::with('user')->with('products')->where('state', 0)->get());
     }
 
     /**
